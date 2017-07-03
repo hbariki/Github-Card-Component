a7b07db041a6eda562b4201d6d5062e8afe68746
Harshini <harshinibariki@gmail.com>
2017-07-02 18:51:17 -0700

    refactor: Add style to function component

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const Card = (props) => {
     return (
         <div>
             <img src = "http://placeholder.it/75" />
-            <div>
-              <div>Name here </div>
+            <div style = {{display: 'inlineBlock', marginLeft : 10}}>
+              <div style = {{fontSize: '1.25em' fontweight:'bold'}}>Name here </div>
               <div>Place here</div>
             </div>
         </div>
     );
 };
-ReactDOM.render(<Card /> , mountNode);
+const CardList = (props) => {
+    return (
+        <div>
+            <Card />
+        </div>
+    );
+}
+ReactDOM.render(<CardList /> , mountNode);
