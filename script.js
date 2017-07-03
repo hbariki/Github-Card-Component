9ee9cfd2aef7d74d21fb3d9d45177d32e18d673e
Harshini <harshinibariki@gmail.com>
2017-07-02 19:49:00 -0700

    feat : Add github users

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const Card = (props) => {
     return (
-        <div>
-            <img src = "http://placeholder.it/75" />
+        <div style = {margin : 1em}>
+            <img width = "75" src = {props.avatar_url} />
             <div style = {{display: 'inlineBlock', marginLeft : 10}}>
-              <div style = {{fontSize: '1.25em' fontweight:'bold'}}>Name here </div>
-              <div>Place here</div>
+              <div style = {{fontSize: '1.25em' fontweight:'bold'}}>{props.name} </div>
+              <div>{props.company}</div>
             </div>
         </div>
     );
 };
+
+let = data [
+  { name : paul O' Shannesy',
+    avatar_url: "https://avatars3.githubusercontent.com/u/1?v=3",
+    company : facebook,
+  },
+  {
+      name: "Ben Alpert",
+      avatar_url: "https://avatars3.githubusercontent.com/u/2?v=3",
+      company: facebook,
+  }
+];
 const CardList = (props) => {
     return (
         <div>
-            <Card />
+          {props.cards.map(card => <Card {...card} />)}
         </div>
     );
 }
-ReactDOM.render(<CardList /> , mountNode);
+ReactDOM.render(<CardList cards = {data} /> , mountNode);
